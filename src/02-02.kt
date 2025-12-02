97aa86f97afae75f26c93772065ce5b592ae12ad
Alexis Ardouin <alexis@helloasso.com>
2025-12-02 17:36:23 +0100

    Solution to advent 02- part 1/2

---
+++ src/02-02.kt
@@ -0,0 +1,19 @@
+import java.io.File
+
+fun main() {
+    val regexInvalidIds = Regex("""(\d+)\1+""")
+    val input = File("res/res-02-01").readLines().first()
+    val results = mutableListOf<Long>()
+
+    val ranges = input.split(",")
+    for (range in ranges) {
+        val (start, end) = range.split("-").map { it.toLong() }
+        for (i in start..end) {
+            if (i.toString().matches(regexInvalidIds)) {
+                println("$i is invalid")
+                results.add(i)
+            }
+        }
+    }
+    println("result is = ${results.sum()}")
+}
